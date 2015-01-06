f1c28fa7d977363b05f077ffe227592d9cb96130
vz8946 <liheng19870711@163.com>
2015-01-06 18:43:00 +0800

    gii allowedIPs

--- config/web.php
+++ config/web.php
@@ -48,7 +48,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = ['class' => 'yii\gii\Module', 'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '61.135.172.68']];
 }
 
 return $config;
